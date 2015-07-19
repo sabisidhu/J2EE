80f6e630be9c547f6250a2dc54f03ca591abf0eb
Sabi <Sabi@DESKTOP-M5L7C2G.Home>
2015-07-19 16:58:46 +0530

    hello

---
+++ Strutr3/src/test/Login.java
@@ -0,0 +1,12 @@
+package test;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.sun.corba.se.spi.orbutil.fsm.ActionBase;
+
+public class Login  {
+	public String exe(){
+		System.out.println("hello");
+		return "";
+	}
+
+}
